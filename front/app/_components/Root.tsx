d8221fd56b682c1335f9b986439267ba5916a47b
Miodini <lucasmondini01@gmail.com>
2025-09-19 00:34:01 -0300

    Added page title

--- front/app/_components/Root.tsx
+++ front/app/_components/Root.tsx
@@ -1,27 +1,55 @@
 'use client'
 import styled from "styled-components"
+import { ThemeProvider, type DefaultTheme } from "styled-components"
 
+import Logo from "./Logo"
+import Side from "./Side"
+import GlobalStyle from "./GlobalStyle"
 // Heights for mobile layout
 const menuHeight = '100px'
 const logoHeight = '75px'
 
-export default styled.div`
-    /* Mobile */
-    height: 100%;
-    display: grid;
-    grid-template-rows: ${logoHeight} ${menuHeight} ${props => props.theme.headerHeight}px 1fr;
-    grid-template-areas: 
-        "logo"
-        "sidepanel"
-        "header"
-        "main";
+const theme: DefaultTheme = {
+  bg: 'rgb(26, 47, 58)',
+  shadow: '0px 0px 15px #0004',
+  asideWidth: 225,
+  headerHeight: 100
+}
 
-    @media (min-width: 768px){
-    /* Desktop */
-        grid-template-columns: ${props => props.theme.asideWidth}px 1fr;
-        grid-template-rows: ${props => props.theme.headerHeight}px 1fr;
-        grid-template-areas: 
-            "logo header"
-            "sidepanel main"
-    }
+const RootDiv = styled.div`
+  /* Mobile */
+  height: 100%;
+  display: grid;
+  grid-template-rows: ${logoHeight} ${menuHeight} ${props => props.theme.headerHeight}px 1fr;
+  grid-template-areas: 
+    "logo"
+    "sidepanel"
+    "header"
+    "main";
+
+  @media (min-width: 768px){
+  /* Desktop */
+    grid-template-columns: ${props => props.theme.asideWidth}px 1fr;
+    grid-template-rows: ${props => props.theme.headerHeight}px 1fr;
+    grid-template-areas: 
+      "logo header"
+      "sidepanel main"
+  }
 `
+
+export default function Root({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <ThemeProvider theme={theme}>
+      <RootDiv>
+        <GlobalStyle />
+        <Logo />
+        <Side />
+        {children}
+     </RootDiv>
+    </ThemeProvider>
+  )
+}
--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,23 +1,16 @@
-'use client'
 import { Montserrat } from 'next/font/google'
-import { ThemeProvider, type DefaultTheme } from "styled-components"
+import { Metadata } from 'next'
 import { config as faConfig } from "@fortawesome/fontawesome-svg-core"
 import '@fortawesome/fontawesome-svg-core/styles.css' // Needed to prevent fa icon huge on load bug
 import 'bootstrap/dist/css/bootstrap.css'
 
 import Root from "./_components/Root"
-import Logo from "./_components/Logo"
-import Side from "./_components/Side"
-import GlobalStyle from "./_components/GlobalStyle"
 import StyledComponentsRegistry from "@/lib/registry"
 
 faConfig.autoAddCss = false
 const montserrat = Montserrat({ subsets: ['latin']})
-const theme: DefaultTheme = {
-  bg: 'rgb(26, 47, 58)',
-  shadow: '0px 0px 15px #0004',
-  asideWidth: 225,
-  headerHeight: 100
+export const metadata: Metadata = {
+  title: 'CRUD'
 }
 
 export default function RootLayout({
@@ -29,14 +22,9 @@ export default function RootLayout({
     <html lang="en" className={montserrat.className}>
       <body>
         <StyledComponentsRegistry>
-          <ThemeProvider theme={theme}>
-            <Root>
-              <GlobalStyle />
-              <Logo />
-              <Side />
-              {children}
-            </Root>
-          </ThemeProvider>
+          <Root>
+            {children}
+          </Root>
         </StyledComponentsRegistry>
       </body>
     </html>
