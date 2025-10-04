0f4bee8a19721a0f628785f6c6978a765ff7fb3a
Lucas Mondini <lhmondini@buckman.com>
2025-10-04 20:22:07 -0300

    Optimizing package.json scripts and setting up e2e test

---
+++ back/jest-e2e.config.ts
@@ -0,0 +1,23 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest'
+
+const config: Config = {
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'js'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  }
+
+}
+
+export default config
