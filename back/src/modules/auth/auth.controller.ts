bc49c1087ff1bde108472520e0e4cfacd6ea7e55
Lucas Mondini <lucasmondini01@gmail.com>
2025-11-20 21:38:45 -0300

    back: fixing 500 error on sync route

--- back/src/modules/auth/auth.controller.ts
+++ back/src/modules/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post } from '@nestjs/common'
+import { BadRequestException, Controller, Req, Post } from '@nestjs/common'
 import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger'
 import { AuthService } from './auth.service'
-import { AccountInfo } from '@/common/decorators/account-info.decorator'
+import { auth } from 'firebase-admin'
 import type { DecodedAccount } from '@/common/types/decoded-account.type'
+import type { Request } from '@/common/types/request.type'
 
 @ApiBearerAuth()
 @Controller('auth')
@@ -13,7 +14,22 @@ export class AuthController {
     description: 'Must be called after login to create the account in the database if it is not yet created'
   })
   @Post('sync')
-  async sync(@AccountInfo() acc: DecodedAccount) {
-    await this.authService.sync(acc)
+  async sync(@Req() request: Request) {
+    const token = this.extractTokenFromHeader(request)
+
+    if (!token) throw new BadRequestException()
+    try {
+      const account = await auth().verifyIdToken(token)
+
+      await this.authService.sync(account as DecodedAccount)
+    } catch {
+      throw new BadRequestException()
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? []
+
+    return type === 'Bearer' ? token : undefined
   }
 }
